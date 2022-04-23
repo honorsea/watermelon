65c9822cc0c5e5c503f484d60453b0112dd5b9d3
Onur <36116897+honorsea@users.noreply.github.com>
2022-04-23 16:09:04 +0300

    support for multiple roms
    ex: node upload.js miles => opens the rom by the nickname of miles which means prosecutor's path here

--- upload.js
+++ upload.js
@@ -1,27 +1,29 @@
 const fs =require("fs");
-const { Buffer } = require('buffer');
 const rcn = require("./config.json");
 const dropboxV2Api = require('dropbox-v2-api');
 const dropbox = dropboxV2Api.authenticate({
     token: rcn.accesstoken
 });
 
-const rom1savefilepath = `./roms/${rcn.roms["prosecutors path"].filename}.sav`;
+//const rom1savefilepath = `./roms/${rcn.roms["prosecutors path"].filename}.sav`;
+let pickednickname = process.argv.splice(2).toString();
+let pickedrom = rcn.roms.find(o => o.nickname === pickednickname);
+console.log(pickedrom)
 
-fs.watchFile(rom1savefilepath, (curr, prev) => { //check if local save file has changed
-    console.log(`${rcn.roms["prosecutors path"].filename} file Changed, uploading now!`);
-    fs.copyFile(`${rom1savefilepath}`, `./roms/saveforupload`, (err) => {
+fs.watchFile(`./roms/${pickedrom.romname}.sav`, (curr, prev) => { //check if local save file has changed
+    console.log(`New save detected for ${pickedrom.romname}, uploading to Dropbox!`);
+    fs.copyFile(`./roms/${pickedrom.romname}.sav`, `./roms/saveforupload`, (err) => {
         if (err) 
             throw err;   
     });
     dropbox({
         resource: 'files/upload',
         parameters: {
-            path: `/Delta Emulator/GameSave-${rcn.roms["prosecutors path"].deeplink}-gameSave`,
+            path: `/Delta Emulator/GameSave-${pickedrom.deeplink}-gameSave`,
             mode: 'overwrite'
             },
         readStream: fs.createReadStream(`./roms/saveforupload`)
     }, (err, result, response) => {
-        console.log(err)
+        //
     });
 });
